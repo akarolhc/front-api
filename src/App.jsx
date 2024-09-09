f9b1d867aa1400589325a34641f7d85ec0350a38
Guilherme Seidel <guilhermekoppe@hotmail.com>
2024-09-09 19:16:13 -0300

    login, footer, header e api

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Login from "./pages/Login";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import ConselhosDiarioApi from "./pages/ConselhoDiarioApi";
+import Api from "./pages/Api";
+
 
 function App() {
   const location = useLocation();  
@@ -18,6 +20,7 @@ function App() {
           <Route path='/' element={<Home />} /> 
           <Route path='/about' element={<About />} /> 
           <Route path='/api-conselho' element={<ConselhosDiarioApi />} /> 
+          <Route path='/apis' element={< Api />} />
         </Routes>
       </div>
       {location.pathname === "/login" && <Footer />} 
